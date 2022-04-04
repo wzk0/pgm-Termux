29a377ad461d339213afdfa1a155496b8a307720
wzk0 <729301277@qq.com>
2022-04-04 21:53:07 +0800

    添加了after

--- after.sh
+++ after.sh
@@ -1,18 +1,19 @@
+#!bin/bash
 ​clear
 printf​ ​"​当前环境:​"
 pwd
 sleep 3s
 printf​ ​"​正在更新容器的apt...​"
-apt-get update -y ​&&​ apt-get upgrade -y
+apt update -y ​&&​ apt upgrade -y
 printf​ ​"​正在安装相应软件...​"
 apt install git python3 python3-pip nano -y
 printf​ ​"​正在获取仓库...​"
 git clone https://gitlab.com/Xtao-Labs/pagermaid-modify.git pgm ​&&​ ​cd​ pgm
 printf​ ​"​正在安装pgm的软件包...​"
-apt-get install imagemagick -y
-apt-get install libzbar-dev -y
-apt-get install tesseract-ocr tesseract-ocr-all -y
-apt-get install redis-server -y
+apt install imagemagick -y
+apt install libzbar-dev -y
+apt install tesseract-ocr tesseract-ocr-all -y
+apt install redis-server -y
 printf​ ​"​正在安装pgm的依赖...​"
 pip3 install -r requirements.txt
 clear
