201f158f3433430e25f13f800f79b7bea811519a
wzk0 <729301277@qq.com>
2022-04-04 21:41:47 +0800

    添加了after

--- after.sh
+++ after.sh
@@ -6,31 +6,22 @@
  ​sleep 3s 
   
  ​printf​ ​"​正在更新容器的apt...​" 
- ​apt-get update -y ​&&​ apt-get upgrade -y 
-  
- ​printf​ ​"​正在安装相应软件...​" 
- ​apt install git python3 python3-pip nano -y 
-  
- ​printf​ ​"​正在获取仓库...​" 
- ​git clone https://gitlab.com/Xtao-Labs/pagermaid-modify.git pgm ​&&​ ​cd​ pgm 
-  
- ​printf​ ​"​正在安装pgm的软件包...​" 
- ​apt-get install imagemagick -y 
- ​apt-get install libzbar-dev -y 
- ​apt-get install tesseract-ocr tesseract-ocr-all -y 
- ​apt-get install redis-server -y 
-  
- ​printf​ ​"​正在安装pgm的依赖...​" 
- ​pip3 install -r requirements.txt 
-  
- ​clear 
-  
- ​printf​ ​"​三秒后将编辑配置文件，请填入相应的api_key和api_hash​" 
- ​cp config.gen.yml config.yml 
-  
- ​sleep 3s 
-  
- ​nano config.yml 
-  
- ​printf​ ​"​正在启动pgm...​" 
- ​python3 -m pagermaid
+apt-get update -y ​&&​ apt-get upgrade -y
+printf​ ​"​正在安装相应软件...​"
+apt install git python3 python3-pip nano -y
+printf​ ​"​正在获取仓库...​"
+git clone https://gitlab.com/Xtao-Labs/pagermaid-modify.git pgm ​&&​ ​cd​ pgm
+printf​ ​"​正在安装pgm的软件包...​"
+apt-get install imagemagick -y
+apt-get install libzbar-dev -y
+apt-get install tesseract-ocr tesseract-ocr-all -y
+apt-get install redis-server -y
+printf​ ​"​正在安装pgm的依赖...​"
+pip3 install -r requirements.txt
+clear
+printf​ ​"​三秒后将编辑配置文件，请填入相应的api_key和api_hash​"
+cp config.gen.yml config.yml
+sleep 3s
+nano config.yml
+printf​ ​"​正在启动pgm...​"
+python3 -m pagermaid
